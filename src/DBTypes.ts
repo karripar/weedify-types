4c7d9e58166df52108ce483c8d6d900536b4d702
karripartanen <karripartanen25@gmail.com>
2025-04-13 15:22:56 +0300

    type added

--- src/DBTypes.ts
+++ src/DBTypes.ts
@@ -59,35 +59,37 @@ type RecipeWithDietaryIds = Recipe & {
 // This type is used to represent a recipe with all fields, including optional ones
 // use with caution, as it may not represent a valid recipe in the database
 type RecipeWithAllFields = {
-  recipe_id?: number;
-  user_id?: number;
-  title?: string;
-  instructions?: string;
-  cooking_time?: number;
-  difficulty_level_id?: number;
-  portions?: number;
-  created_at?: string;
-  filename?: string;
-  filesize?: number;
-  media_type?: string;
-  screenshots?: string[];
-  thumbnail?: string | null;
-  difficulty_level?: string;
-  diet_types?: [
+  recipe_id: number;
+  user_id: number;
+  title: string;
+  instructions: string;
+  cooking_time: number;
+  difficulty_level_id: number;
+  portions: number;
+  created_at: string;
+  filename: string;
+  filesize: number;
+  media_type: string;
+  screenshots: string[];
+  thumbnail: string | null;
+  difficulty_level: string;
+  diet_types: [
     {
-      diet_type_id?: number;
-      name?: string
+      diet_type_id: number;
+      name: string
     }
   ],
-  ingredients?: [
+  ingredients: [
     {
-      ingredient_id?: number;
-      name?: string;
-      amount?: number;
-      unit?: string;
+      ingredient_id: number;
+      name: string;
+      amount: number;
+      unit: string;
     }
   ]
 }
+
+type PartialFullRecipe = Partial<RecipeWithAllFields>;
 
 type Favorite = {
   favorite_id: number;
@@ -304,5 +306,6 @@ export type {
   UserWithDietaryIds,
   UserCheck,
   ResetToken,
-  RecipeWithAllFields
+  RecipeWithAllFields,
+  PartialFullRecipe
 };
